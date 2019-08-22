7f4e5c373eac447580d222051f6682496c05e211
bhavyapanchmatiya <53083649+bhavyapanchmatiya@users.noreply.github.com>
2019-08-22 15:13:55 +0100

    Add files via upload

---
+++ LoopsAndArraysHomework/src/Q21RightAngleTriangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Q21RightAngleTriangle {
+
+public static void main(String[]args){
+
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter any number = ");
+
+    int A = scanner.nextInt();
+
+    for(int i =1;i<=A;i++){
+
+        for(int j = 1;j<=i;j++)
+
+            System.out.print("@");
+
+        {
+            System.out.println();
+
+    }
+
+
+
+
+
+}
+
+
+
+}}
